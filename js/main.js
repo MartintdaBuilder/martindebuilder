820c1aed2c1c83887fb9cc877711eeda451532fd
Woler <hristo_den@abv.bg>
2022-07-22 01:23:22 +0300

    added tons of features mostly settings.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,55 @@
-//Test Commit
-let a = 4;
+function setupSettings() {
+    document.getElementById("title").textContent = title;
+    document.getElementById("title").style.color = titleColor;
+
+    let centerCard = document.getElementById("card").style;
+    let leftSide = document.getElementById("leftSpace").style;
+    let rightSide = document.getElementById("rightSpace").style;
+
+    setSideBackground(centerCard, cardImage);
+    setSideBackground(leftSide, leftImage);
+    setSideBackground(rightSide, rightImage);
+}
+
+function getAdditionalButtons() {
+    let buttonList = document.getElementById("down");
+    buttons.forEach(button => {
+        let linkToAdd = document.createElement("a");
+        let buttonToAdd = document.createElement("button");
+
+        linkToAdd.href = button.btnLink;
+        linkToAdd.draggable = false;
+        linkToAdd.target = "_blank";
+        linkToAdd.classList.add("link-none", "w-75", "my-3");
+
+        buttonToAdd.type = "button";
+        buttonToAdd.classList.add("btn", "rounded-pill", "py-3", "w-100");
+        buttonToAdd.textContent = button.btnText;
+
+        setBackground(buttonToAdd, button.btnColor);
+
+        buttonToAdd.style.color = button.btnTextColor;
+
+        buttonList.appendChild(linkToAdd);
+        buttonList.lastChild.appendChild(buttonToAdd);
+    });
+}
+
+function setBackground(obj, bg) {
+    obj.style.background = bg;
+    obj.style.background = "-moz-" + bg;
+    obj.style.background = "-webkit-" + bg;
+    obj.style.background = "-moz-" + bg;
+}
+
+function setSideBackground(obj, dataObj) {
+    obj.backgroundColor = dataObj.fallbackColor;
+    obj.backgroundImage = "url('res/" + dataObj.fileNameAndExt + "')";
+    obj.backgroundPositionX = dataObj.offsetX + "%";
+    obj.backgroundPositionY = dataObj.offsetY + "%";
+    obj.transform = "scaleX(" + -1 * ((dataObj.mirror * 2) - 1) + ")";
+}
+
+setupSettings();
+getAdditionalButtons();
+console.log();
---
+++ js/settings.js
@@ -0,0 +1,58 @@
+const title = "Martin the Builder";
+//default color "#202529"
+const titleColor = "white";
+
+const cardImage = {
+	fileNameAndExt: "diamonds.png",
+	//can be gradient
+	fallbackColor: "white",
+	offsetX: 500,
+	offsetY: 0,
+	mirror: false
+};
+
+const leftImage = {
+	fileNameAndExt: "guuji_of_the_grand_narukami_shrine.png",
+	//can be gradient
+	fallbackColor: "deeppink",
+	offsetX: 50,
+	offsetY: 0,
+	mirror: false
+};
+
+const rightImage = {
+	fileNameAndExt: "guuji_of_the_grand_narukami_shrine.png",
+	//can be gradient
+	fallbackColor: "deeppink",
+	offsetX: 50,
+	offsetY: 0,
+	mirror: true
+};
+
+const buttons = [{
+	btnText: "Twitter",
+	btnLink: "https://twitter.com/martindabuilder",
+	btnColor: "#1da1f2",
+	btnTextColor: "#ffffff"
+},{
+	btnText: "Instagram",
+	btnLink: "https://www.instagram.com/martindabuilder",
+	btnColor: "linear-gradient(45deg, #f09433 0%, #e6683c 25%, #dc2743 50%, #cc2366 75%, #bc1888 100%)",
+	btnTextColor: "#ffffff"
+},{
+	btnText: "Pixiv",
+	btnLink: "https://www.pixiv.net/en/users/69814881",
+	btnColor: "#0077c7",
+	btnTextColor: "#ffffff"
+},{
+	btnText: "Artstation",
+	btnLink: "https://www.artstation.com/martinthebuilder",
+	btnColor: "#14aff0",
+	btnTextColor: "#ffffff"
+},{
+	btnText: "Ko-Fi",
+	btnLink: "https://ko-fi.com/martinthebuilder80998#paypalModal",
+	btnColor: "#ff5e5b",
+	btnTextColor: "#ffffff"
+}];
+
